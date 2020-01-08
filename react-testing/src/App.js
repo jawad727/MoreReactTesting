9c406fdaf8492df432fea331912f2449f69bd51c
jawad727 <jawaddhussein@gmail.com>
2020-01-07 20:31:51 -0800

    basic styling with logo created

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Header from "./components/header/header.js"
 
 function App() {
   return (
     <div>
-
-
+        <Header />
     </div>
   );
 }
---
+++ react-testing/src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles.scss';
+
+const Header = (props) => {
+    return (
+        <header>
+            <div className="wrap">
+            <div className="logo">
+                <h1>MyLogo</h1>
+            </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
